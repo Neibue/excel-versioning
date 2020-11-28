2851c967db289b4eb55878455c50092df243ee15
Neibue <devwithmax@outlook.com>
2020-11-28 14:50:24 +0100

    Add project files.

---
+++ excel-versioning/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace excel_versioning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var arg in args) {
+                Console.WriteLine("Mes arguments : " + arg);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
